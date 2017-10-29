d3ed7c1e22ccc0c9160832e0cb83587121874bdd
root <root@localhost.localdomain>
2017-10-29 02:34:35 +0000

    created new file and folders

---
+++ firstprog.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  int i;
+  for(i=0; i<10; i++) //loop 10 times
+  {
+    puts("Hello World\n"); //print to the output
+  }
+  return 0; // Tell the os to exit without error
+}
