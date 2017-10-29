fecdc7e4815de1dff9c47c2f0ce7781b2bb6d42f
root <root@localhost.localdomain>
2017-10-29 03:24:41 +0000

    committed

--- firstprog.c
+++ firstprog.c
@@ -5,7 +5,7 @@ int main()
   int i;
   for(i=0; i<10; i++) //loop 10 times
   {
-    puts("Hello World\n"); //print to the output
+    puts("Hello, world\n"); //print to the output
   }
   return 0; // Tell the os to exit without error
 }
