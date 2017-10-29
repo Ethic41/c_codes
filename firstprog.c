82cdea8c1420e89446c82a375033e4bded0bc307
root <root@localhost.localdomain>
2017-10-29 04:39:20 +0000

    used "printf" instead of puts

--- firstprog.c
+++ firstprog.c
@@ -5,7 +5,7 @@ int main()
   int i;
   for(i=0; i<10; i++) //loop 10 times
   {
-    puts("Hello, world\n"); //print to the output
+    printf("Hello, world!\n"); //print to the output
   }
   return 0; // Tell the os to exit without error
 }
