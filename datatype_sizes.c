5db8454c461c82b175475b9757f90361ba5777e7
root <root@localhost.localdomain>
2017-11-05 22:13:28 +0100

    initial commit

---
+++ datatype_sizes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  printf("the 'int' data type size is \t\t %d bytes\n", sizeof(int));
+  printf("the 'unsigned int' data type size is \t\t %d bytes\n", sizeof(unsigned int));
+  printf("the 'short int' data type size is \t\t %d bytes\n", sizeof(short int));
+  printf("the 'long int' data type size is \t\t %d bytes\n", sizeof(long int));
+  printf("the 'long long int' data type size is \t\t %d bytes\n", sizeof(long long int));
+  printf("the 'float' data type size is \t\t %d bytes\n", sizeof(float));
+  printf("the 'char' data type size is \t\t %d bytes\n", sizeof(char));
+}
